6f1551b0eeb307a12bc0a81d85c1d3cd70ef8314
monstercoder <monstercoder@gmail.com>
2023-05-02 23:35:59 -0500

    add pdf extraction

--- src/api/Controllers/FileUploadController.cs
+++ src/api/Controllers/FileUploadController.cs
@@ -1,4 +1,9 @@
+using Azure;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.TextExtractor;
 
 namespace app.Controllers;
 
@@ -7,10 +12,14 @@ namespace app.Controllers;
 public class FileUploadController : ControllerBase
 {
     private readonly ILogger<FileUploadController> _logger;
+    private readonly IHubContext<ProgressHub> _hubContext;
+    private readonly IConfiguration _config;
 
-    public FileUploadController(ILogger<FileUploadController> logger)
+    public FileUploadController(ILogger<FileUploadController> logger, IHubContext<ProgressHub> hubContext, IConfiguration config)
     {
         _logger = logger;
+        _hubContext = hubContext;
+        _config = config;
     }
 
     [HttpPost]
@@ -22,15 +31,47 @@ public class FileUploadController : ControllerBase
         }
 
         // Save the file to a temporary location
-        string tempFilePath = Path.GetTempFileName();
-        using (var stream = System.IO.File.Create(tempFilePath))
-        {
-            await file.CopyToAsync(stream);
-        }
-
+    //    using (var stream = System.IO.File.Create(tempFilePath))
+    //     {
+    //         await file.CopyToAsync(stream);
+    //     }     string tempFilePath = Path.GetTempFileName();
+    
+        var stream = new MemoryStream();
+        await file.CopyToAsync(stream);
+        stream.Position =0;
         // Start the background job to extract content from the PDF
-        //_ = Task.Run(async () => await ExtractPdfContent(tempFilePath, file.FileName));
+        _ = Task.Run(async () => await ExtractPdfContent(stream, file.FileName));
 
          return Ok($"PDF file uploaded successfully. Content extraction is in progress.{file.FileName}");
+    }
+
+    private async Task ExtractPdfContent(Stream stream, string fileName)
+    {
+        // Update progress
+        await _hubContext.Clients.All.SendAsync("ProgressUpdate", JsonConvert.SerializeObject(new { Progress = 0, FileName = fileName }));
+
+        try
+        {
+            var pdf = PdfDocument.Open(stream);
+            var result = String.Empty;
+            foreach (var page in pdf.GetPages())
+            {
+                // Either extract based on order in the underlying document with newlines and spaces.
+                var text = ContentOrderTextExtractor.GetText(page);
+                var progress = (page.Number * 100 / pdf.NumberOfPages);
+                await _hubContext.Clients.All.SendAsync("ProgressUpdate", JsonConvert.SerializeObject(new { Progress = progress, FileName = $"p:{page.Number}" }));
+                result = $"{result}{text}";
+                _logger.LogInformation($"Indexed the PDF content successfully: {result}");
+            }
+
+            // Save the extracted text in a file with the same filename and the .ext extension
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Error indexing the PDF content: {ex}");
+        }
+
+        // Update progress
+        await _hubContext.Clients.All.SendAsync("ProgressUpdate", JsonConvert.SerializeObject(new { Progress = 100, FileName = fileName }));
     }
 }
--- src/api/Program.cs
+++ src/api/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSignalR();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,5 +22,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHub<ProgressHub>("/progressHub");
 
 app.Run();
